673abb2c4cdbd700249ded6356ce38f1f99be07a
AJ Sorbello <ajsorbello@gmail.com>
2024-09-05 09:44:27 -0700

    created login logout

--- src/recipe_project/urls.py
+++ src/recipe_project/urls.py
@@ -19,10 +19,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from .views import login_view, logout_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("recipe.urls")),
+    path("login/", login_view, name="login"),
+    path('logout/', logout_view, name='logout'),
 ]
 
 if settings.DEBUG:
